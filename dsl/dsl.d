2ef65bda0576c3c7039a1948ebaadf664f8e7aff
munchkinhalfling <munchkinhalfling@pm.me>
2020-06-12 17:26:31 -0400

    Create DSL

---
+++ dsl/dsl.d
@@ -0,0 +1,35 @@
+module dsl.dsl;
+import std.string;
+
+struct EPDSLDecl {
+    private string[string] opts;
+    public string name;
+    public string value;
+    public string getVal(string key) {
+        return opts[key];
+    }
+}
+class EPDSLInstance {
+    public EPDSLDecl[] decls;
+    public static EPDSLInstance parse(string data) {
+        foreach(line; splitLines(data)) {
+            EPDSLDecl decl;
+            string[] parts = line.split(" ");
+            decl.name = parts[0];
+            decl.value = parts[1];
+            string[] optParts = parts[2 .. $];
+            string[] optKeys;
+            string[] optVals;
+            for(int i = 0; i < optParts.length; i++) {
+                if(i % 2 == 0) {
+                    optKeys ~= [optParts[i]];
+                } else {
+                    optVals ~= [optParts[i]];
+                }
+            }
+            for(int i = 0; i < optKeys.length; i++) {
+                decl.opts[optKeys[i]] = optVals[i];
+            }
+        }
+    }
+}
